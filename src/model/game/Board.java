1bf55f2a198406348aafff21fbf622e375c167e7
Abolfazl <mr.abolfazl81@gmail.com>
2022-03-09 18:42:21 +0330

    initial table colors debugged

--- src/model/game/Board.java
+++ src/model/game/Board.java
@@ -10,8 +10,8 @@ public class Board {
     public Board(Function function) {
         this.function = function;
         table = new Cell[size][size];
-        for (int i = 0; i < size; i++)
-            for (int j = 0; j < size; j++) {
+        for (int i = size - 1; i >= 0; i--)
+            for (int j = size - 1; j >= 0; j--) {
                 Color color = Color.getColorByNumber(this.function.nextInt());
                 table[i][j] = new Cell(i, j, new Candy(color));
             }
